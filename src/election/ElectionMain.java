b805a0c6baef73b45f277f159cda65f0053fa9f7
Administrator <Administrator@kwak-PC>
2015-12-04 12:11:18 +0900

    학생 사진 들고와서 표시하기

--- src/election/ElectionMain.java
+++ src/election/ElectionMain.java
@@ -2,6 +2,7 @@ package election;
 
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
@@ -10,7 +11,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Calendar;
 import java.util.Enumeration;
 import java.util.List;
 import javax.comm.CommPortIdentifier;
@@ -18,6 +21,7 @@ import javax.comm.SerialPort;
 import javax.comm.SerialPortEvent;
 import javax.comm.SerialPortEventListener;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -31,6 +35,7 @@ public class ElectionMain implements Runnable{
 	Thread readThread;
 	
 	JFrame jFrame = new JFrame("선거");
+	JPanel topPanel = new JPanel(new GridLayout(2,1));
 	JPanel northPanel = new JPanel(new FlowLayout());
 	String stringLabel[] = {"성 명","학년","반","번호","과"};
 	int textFiled[] = {10,2,2,2,30};
@@ -40,21 +45,46 @@ public class ElectionMain implements Runnable{
 	String sql = "";
 	
 	public ElectionMain() {
-		/*JPanel imagePanel = new JImagePanel();
-		imagePanel.setSize(100,100);
-		jFrame.add(imagePanel);*/
 		JPanel elecTitle = new JPanel(new FlowLayout());
+		elecTitle.setSize(JFrame.MAXIMIZED_HORIZ, 100);
+		Calendar c =  Calendar.getInstance();
+		String year = String.valueOf(c.get(Calendar.YEAR));
+		JLabel yearLabel = new JLabel(year);
+		elecTitle.add(yearLabel);
 		JLabel titleLabel = new JLabel("년도");
-		JLabel titleLabel2 = new JLabel("선거 투표");
 		elecTitle.add(titleLabel);
+		con = dbConn();
+		sql = "SELECT subject FROM Election_Cand WHERE sort=1";
+		try {
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				JLabel elecLabel = new JLabel(rs.getString("subject"));
+				elecTitle.add(elecLabel);
+			} else {
+				System.out.println("a");
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+
+		JLabel titleLabel2 = new JLabel("선거 투표");
 		elecTitle.add(titleLabel2);
-		jFrame.add(elecTitle);
+		topPanel.add(elecTitle);
 		
 		for(int i=0; i<stringLabel.length;i++) {
 			northPanel.add(new JLabel(stringLabel[i]));
 			northPanel.add(new JTextField(textFiled[i]));
 		}
-		jFrame.add(northPanel);
+		topPanel.add(northPanel);
+		jFrame.add(topPanel,"North");
+		
+		JPanel centerPanel = new JPanel(new FlowLayout());
+		JLabel checkLabel = new JLabel("학생증을 체크해주세요!");
+		centerPanel.add(checkLabel);
+		
+		jFrame.add(centerPanel);
+		
 		jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jFrame.setVisible(true);
@@ -83,12 +113,16 @@ public class ElectionMain implements Runnable{
 						    int numBytes = input.read(readBuffer);
 				    	}
 				    	String rfcard = new String(readBuffer,1,10);
-				    	con = dbConn();
 						sql = "SELECT class,ban,num,name FROM student WHERE rf_card_num='"+rfcard+"'";
 						Statement stmt = con.createStatement();
 						rs = stmt.executeQuery(sql);
 						if(rs.next()) {
-							
+							centerPanel.remove(checkLabel);
+							JPanel elecImage= new JPanel(new GridLayout(1, 3));
+							elecImage.add(new JButton("테스트"));
+							centerPanel.add(elecImage);
+							jFrame.setVisible(false);
+							jFrame.setVisible(true);
 						} else {
 							System.out.println("b");
 						}

9366303f2c2fb45e12cc1dc6d49fd7e537fd242b
Administrator <Administrator@admin-PC>
2015-12-08 00:47:26 +0900

    투표까지 됌

--- src/election/ElectionMain.java
+++ src/election/ElectionMain.java
@@ -1,32 +1,30 @@
 package election;
 
 import java.awt.FlowLayout;
-import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
+import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Calendar;
 import java.util.Enumeration;
 import java.util.List;
-import javax.comm.CommPortIdentifier;
-import javax.comm.SerialPort;
-import javax.comm.SerialPortEvent;
-import javax.comm.SerialPortEventListener;
+import javax.comm.*;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 
 public class ElectionMain implements Runnable{
 	static CommPortIdentifier portId;
@@ -34,7 +32,7 @@ public class ElectionMain implements Runnable{
 	InputStream input;
 	SerialPort serialPort;
 	Thread readThread;
-	JImagePanel imagePanel = new JImagePanel();
+	
 	
 	JFrame jFrame = new JFrame("선거");
 	JPanel topPanel = new JPanel(new GridLayout(2,1));
@@ -43,13 +41,15 @@ public class ElectionMain implements Runnable{
 	JTextField jTextFieldClass= new JTextField(2);
 	JTextField jTextFieldBan = new JTextField(2);
 	JTextField jTextFieldNum = new JTextField(2);
+	JTextField elecText = new JTextField(2);
+	JLabel elecLabel = new JLabel("기호");
 	Connection con;
 	PreparedStatement pstmt;
 	ResultSet rs, rs2, rs3;
-	String sql = "";
+	String sql = "", e_st = "";
 	String[] splitString;
 	
-	public ElectionMain() {
+	public ElectionMain() { 
 		JPanel elecTitle = new JPanel(new FlowLayout());
 		elecTitle.setSize(JFrame.MAXIMIZED_HORIZ, 100);
 		Calendar c =  Calendar.getInstance();
@@ -73,6 +73,8 @@ public class ElectionMain implements Runnable{
 		northPanel.add(jTextFieldBan);
 		northPanel.add(new JLabel("번 호"));
 		northPanel.add(jTextFieldNum);
+		northPanel.add(elecLabel);
+		northPanel.add(elecText);
 		
 		topPanel.add(northPanel);
 		jFrame.add(topPanel,"North");
@@ -81,7 +83,7 @@ public class ElectionMain implements Runnable{
 		JLabel checkLabel = new JLabel("학생증을 체크해주세요!");
 		centerPanel.add(checkLabel);
 		
-		jFrame.add(centerPanel);
+		jFrame.add(centerPanel,"Center");
 		
 		jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -110,44 +112,101 @@ public class ElectionMain implements Runnable{
 				    	while (input.available() > 0) {
 						    int numBytes = input.read(readBuffer);
 				    	}
-				    	String rfcard = new String(readBuffer,1,10);
+				    	String rfcard = new String(readBuffer,0,10);
+				    	String st_id;
 				    	con = dbConn();
-						sql = "SELECT name,class,ban,num FROM student WHERE rf_card_num='"+rfcard+"'";
+						sql = "SELECT name,class,ban,num,st_id FROM student WHERE rf_card_num='"+rfcard+"'";
 						Statement stmt = con.createStatement();
 						rs = stmt.executeQuery(sql);
 						if(rs.next()) {
 							centerPanel.remove(checkLabel);
 							jTextFieldName.setText(rs.getString("name"));
+							jTextFieldName.setEditable(false);
 							jTextFieldClass.setText(String.valueOf(rs.getInt("class")));
+							jTextFieldClass.setEditable(false);
 							jTextFieldBan.setText(String.valueOf(rs.getInt("ban")));
+							jTextFieldBan.setEditable(false);
 							jTextFieldNum.setText(String.valueOf(rs.getInt("num")));
-							String sql2 = "SELECT st_id FROM Election_Cand WHERE subject='1학년 학생회장'";
-							pstmt = con.prepareStatement(sql2);
-							rs2 = pstmt.executeQuery();
+							jTextFieldNum.setEditable(false);
+							st_id = rs.getString("st_id");
+							sql = "SELECT st_id FROM Election_Cand WHERE subject='1학년 학생회장'";
+							pstmt = con.prepareStatement(sql);
+							rs = pstmt.executeQuery();
 							int size = 0;
-							while(rs2.next()){
+							while(rs.next()) {
 								size++;
 							}
-							JPanel[] panelImage = new JPanel[size];
-							JPanel elecImage= new JPanel(new GridLayout(1, 3));
-							String sql3 = "SELECT st_id FROM Election_Cand WHERE subject='1학년 학생회장'";
-							pstmt = con.prepareStatement(sql3);
-							rs3 = pstmt.executeQuery();
-							while(rs3.next()) {
-								panelImage[rs3.getRow()-1] = imagePanel.getPanel(rs3.getString("st_id"));
-								elecImage.add(panelImage[rs3.getRow()-1]);
+							pstmt = con.prepareStatement(sql);
+							rs2 = pstmt.executeQuery();
+							String e_st_id[] = new String[size];
+							JPanel elecImage= new JPanel(new FlowLayout());
+							while(rs2.next()) {
+								e_st_id[rs2.getRow()-1] = rs2.getString("st_id");
+								String filePath = "C:\\Uni_cool\\image\\"+rs2.getString("st_id")+".jpg";
+								ImageIcon icon = new ImageIcon(filePath);
+								if(icon != null) {
+									ImageIcon thumbnailIcon = new ImageIcon(getScaledImage(icon.getImage(), 200, 200));
+									elecImage.add(new JLabel(thumbnailIcon));
+								}
+								centerPanel.add(elecImage);
 							}
-							centerPanel.add(elecImage);
-							jFrame.setVisible(false);
+							
+							
+							jFrame.add(centerPanel);
+							elecText.requestFocus();
+							elecText.addKeyListener(new KeyListener() {
+								@Override
+								public void keyTyped(KeyEvent e) {
+								}
+								@Override
+								public void keyReleased(KeyEvent e) {
+								}
+								
+								@Override
+								public void keyPressed(KeyEvent e) {
+									switch (e.getKeyCode()) {
+									case 97:
+										e_st=e_st_id[0];
+										break;
+									case 98:
+										e_st=e_st_id[1];
+										break;
+									case 99:
+										e_st=e_st_id[2];
+										break;
+									default:
+										break;
+									}
+									if(e.getKeyCode() == 10) {
+										sql = "INSERT INTO Election_list(e_sel, st_id, e_st_id, e_date, e_time) VALUES ('A','"+st_id+"','"+e_st+"',getdate(),'000000')";
+										try {
+											pstmt = con.prepareStatement(sql);
+											pstmt.executeUpdate();
+										} catch (SQLException e1) {
+											e1.printStackTrace();
+										}
+									}
+								}
+							});
+							//jFrame.setVisible(false);
 							jFrame.setVisible(true);
 						} else {
-							System.out.println("b");
+							//System.out.println("b");
 						}
 				    } catch (Exception e) {
 				    	e.printStackTrace();
 				    }
 				    break;
 					}
+				}
+
+				private Image getScaledImage(Image image, int i, int j) {
+					BufferedImage resizedImg = new BufferedImage(i, j, BufferedImage.TYPE_INT_RGB);
+			        Graphics2D g2 = resizedImg.createGraphics();
+			        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			        g2.drawImage(image, 0, 0, i, j, null);
+			        g2.dispose();
+			        return resizedImg;
 				}
 			});
 		    serialPort.notifyOnDataAvailable(true);
